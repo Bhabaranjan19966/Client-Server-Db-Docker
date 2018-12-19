075a3891ddce54a5c42e6ac6db36e7168994e8f5
Bhabaranjan19966 <42641634+Bhabaranjan19966@users.noreply.github.com>
2018-12-19 17:51:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <a className="App-link" id="loginButton" href={`/auth`}> Connect to Gitlab </a>
           {/* <button onClick={this.test}> test </button> */}
         </header>
